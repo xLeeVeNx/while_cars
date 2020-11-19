6c49061443d41472155104d9cfd8ed12058c16bf
Ramil Ashrafulin <HEHAHA@bk.ru>
2020-11-20 00:28:08 +0600

    feat: logo, scroll

--- scripts/index.js
+++ scripts/index.js
@@ -62,3 +62,18 @@ $(document).ready(function() {
         centerMode: true,
     });
 });
+
+
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
