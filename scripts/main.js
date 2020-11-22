8a64f386ddb76c11c3f09e1583b4b8e699122205
xLeeVeNx <berdyev.damil1@mail.ru>
2020-11-22 16:10:59 +0600

    added filter

--- scripts/main.js
+++ scripts/main.js
@@ -42,25 +42,9 @@ $("[data-fancybox]").fancybox({
   closeClick: true,
 });
 
-var mixer = mixitup(".assortment__grid", {
-  animation: {
-    effects: "fade translateZ(-360px) stagger(34ms)",
-  },
-});
-
 jQuery(document).ready(function ($) {
-  //open/close lateral filter
-  $(".cd-filter-trigger").on("click", function () {
-    triggerFilter(true);
-  });
-  $(".cd-filter .cd-close").on("click", function () {
-    triggerFilter(false);
-  });
-
   function triggerFilter($bool) {
     var elementsToTrigger = $([
-      $(".cd-filter-trigger"),
-      $(".cd-filter"),
       $(".cd-tab-filter"),
       $(".cd-gallery"),
     ]);
@@ -103,14 +87,6 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  //close filter dropdown inside lateral .cd-filter
-  $(".cd-filter-block h4").on("click", function () {
-    $(this)
-      .toggleClass("closed")
-      .siblings(".cd-filter-content")
-      .slideToggle(300);
-  });
-
   //fix lateral filter and gallery on scrolling
   $(window).on("scroll", function () {
     !window.requestAnimationFrame
@@ -149,8 +125,6 @@ jQuery(document).ready(function ($) {
     },
   });
 
-  //search filtering
-  //credits http://codepen.io/edprats/pen/pzAdg
   var inputText;
   var $matching = $();
 
@@ -161,51 +135,18 @@ jQuery(document).ready(function ($) {
       timer = setTimeout(callback, ms);
     };
   })();
-
-  $(".cd-filter-content").hide();
-
-  $(".cd-filter-content input[type='search']").keyup(function () {
-    // Delay function invoked to make sure user stopped typing
-    delay(function () {
-      inputText = $(".cd-filter-content input[type='search']")
-        .val()
-        .toLowerCase();
-      // Check to see if input field is empty
-      if (inputText.length > 0) {
-        $(".mix").each(function () {
-          var $this = $(this);
-
-          // add item to be filtered out if input text matches items inside the title
-          if ($this.attr("class").toLowerCase().match(inputText)) {
-            $matching = $matching.add(this);
-          } else {
-            // removes any previously matched item
-            $matching = $matching.not(this);
-          }
-        });
-        $(".cd-gallery ul").mixItUp("filter", $matching);
-      } else {
-        // resets the filter to show all item if input is empty
-        $(".cd-gallery ul").mixItUp("filter", "all");
-      }
-    }, 200);
-  });
+  
 });
 
-/*****************************************************
-	MixItUp - Define a single object literal 
-	to contain all filter custom functionality
-*****************************************************/
+
 var buttonFilter = {
-  // Declare any variables we will need as properties of the object
   $filters: null,
   groups: [],
   outputArray: [],
   outputString: "",
 
-  // The "init" method will run on document ready and cache any jQuery objects we will need.
   init: function () {
-    var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "buttonFilter" object so that we can share methods and properties between all parts of the object.
+    var self = this; 
 
     self.$filters = $(".cd-main-content");
     self.$container = $(".cd-gallery ul");
@@ -223,7 +164,6 @@ var buttonFilter = {
     self.bindHandlers();
   },
 
-  // The "bindHandlers" method will listen for whenever a button is clicked.
   bindHandlers: function () {
     var self = this;
 
@@ -238,19 +178,11 @@ var buttonFilter = {
   parseFilters: function () {
     var self = this;
 
-    // loop through each filter group and grap the active filter from each one.
     for (var i = 0, group; (group = self.groups[i]); i++) {
       group.active = [];
       group.$inputs.each(function () {
         var $this = $(this);
-        if (
-          $this.is('input[type="radio"]') ||
-          $this.is('input[type="checkbox"]')
-        ) {
-          if ($this.is(":checked")) {
-            group.active.push($this.attr("data-filter"));
-          }
-        } else if ($this.is("select")) {
+        if ($this.is("select")) {
           group.active.push($this.val());
         } else if ($this.find(".selected").length > 0) {
           group.active.push($this.attr("data-filter"));
@@ -263,16 +195,14 @@ var buttonFilter = {
   concatenate: function () {
     var self = this;
 
-    self.outputString = ""; // Reset output string
+    self.outputString = ""; 
 
     for (var i = 0, group; (group = self.groups[i]); i++) {
       self.outputString += group.active;
     }
 
-    // If the output string is empty, show all rather than none:
     !self.outputString.length && (self.outputString = "all");
 
-    // Send the output string to MixItUp via the 'filter' method:
     if (self.$container.mixItUp("isLoaded")) {
       self.$container.mixItUp("filter", self.outputString);
     }
