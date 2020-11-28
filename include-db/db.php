e68480d862a70d2fcfa45a90b3eab7689026f29d
xLeeVeNx <berdyev.damil1@mail.ru>
2020-11-28 19:15:03 +0600

    Created a project - WhileCars

--- include-db/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $conn = new PDO("mysql:host=localhost;dbname=while_cars", 'mysql', 'mysql');
-
-  $query = "INSERT INTO clientinfo VALUES(NOW(), :phone, :email, :name)";
-  $msg = $conn->prepare($query);
-  $msg->execute(['phone' => $_POST['phone'], 'email' => $_POST['email'], 'name' => $_POST['name']]);
-?>
